fee324e3f535da9183d03e3414619f8e49082971
brawyl <bwylie@ualberta.ca>
2020-09-28 18:45:21 -0600

    Adds world map and hero movement in overworld

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    void Awake()
+    {
+        //check if instance exists
+        if (instance == null)
+        {
+            //set instance to this
+            instance = this;
+        }
+        //exists but is another instance
+        else if (instance != this)
+        {
+            //destroy it
+            Destroy(gameObject);
+        }
+        //set this to not be destroyable
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour
+{
+    float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0f, moveZ);
+        GetComponent<Rigidbody>().velocity = movement * moveSpeed;
+    }
+}
